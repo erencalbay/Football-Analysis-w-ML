6b6ace5ccad7de5ea9cf68ce2e9d465cf6f78263
Eren Çalbay <calbayeren@gmail.com>
2025-01-05 22:28:31 +0300

    Feat: Team color assignment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import read_video, save_video
 from trackers import Tracker
+from team_assigner import TeamAssigner
+import cv2
 
 def main():
     #Read Video
@@ -7,13 +9,40 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
     print("okuyor")
 
-    #Initialize Tracker
 
+    #Initialize Tracker
+    
     tracker = Tracker('models/best.pt')
 
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
+    
+    # Assign Player Teams
+
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], tracks['players'][0])
+
+    for frame_num, player_truck in enumerate(tracks['players']):
+        for player_id, track in player_truck.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track['bbox'], player_id)
+
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['color'] = team_assigner.team_colors[team]
+
+    
+    # save croppeed image of a player
+    for track_id, player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+
+        # crop bbox from frame
+        cropped_frame = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        # save the cropped image
+        cv2.imwrite(f'output_videos/player.jpg', cropped_frame)
+        break
+
     
     #Draw Output
     ## Draw Object Tracks
--- trackers/tracking.py
+++ trackers/tracking.py
@@ -152,7 +152,8 @@ class Tracker:
 
             #Draw Players
             for track_id, player in player_dict.items():
-                frame = self.draw_ellipse(frame, player["bbox"], (0, 255, 0), track_id)
+                color = player.get("team_color", (0, 255, 0))
+                frame = self.draw_ellipse(frame, player["bbox"], color, track_id)
 
             #Draw Referree
             for _, referee in referee_dict.items():
